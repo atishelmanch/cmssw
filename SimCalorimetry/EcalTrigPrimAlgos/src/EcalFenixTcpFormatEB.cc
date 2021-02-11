28434c46a1fe664e4b27e27a2d2a2a511bb007c8
Davide Valsecchi <davide.valsecchi@cern.ch>
2021-02-11 09:03:42 +0100

    Restored case 2 in EB TCP formatter output

--- SimCalorimetry/EcalTrigPrimAlgos/src/EcalFenixTcpFormatEB.cc
+++ SimCalorimetry/EcalTrigPrimAlgos/src/EcalFenixTcpFormatEB.cc
@@ -75,7 +75,7 @@ void EcalFenixTcpFormatEB::process(std::vector<int> &Et_even_sum,
             myEt = Et_even_sum[i];
           }
           break;
-        case 3: // output even+odd 
+        case 2: // output even+odd 
           myEt = Et_even_sum[i] + Et_odd_sum[i];
           break;
       }
